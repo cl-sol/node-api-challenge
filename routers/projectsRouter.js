e24efad2554564507934a922014853b7524fcbbd
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-08-16 21:21:42 -0400

    projects CRUD complete

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -1,7 +1,7 @@
 //imports
 const express = require("express");
 const Projects = require("../data/helpers/projectModel");
-// const actions = require("../data/helpers/actionModel");
+const actions = require("../data/helpers/actionModel");
 
 //router
 const router = express.Router();
@@ -20,5 +20,74 @@ router.get("/", (req, res) => {
         })
 });
 
-module.exports = router;
+router.get("/:id", (req, res)=> {
+    Projects.getProjectActions(req.params.id)
+    .then(project => {
+        if(project === 0) {
+            res.status(400).json({
+                message: "User not found"
+            })
+        } else {
+            res.status(200).json(project)
+        }
+    })
+})
 
+router.get("/:id/action", (req, res) => {
+    Projects.getProjectActions(req.params.id)
+        .then(action => {
+            if(action === 0 ) {
+                res.status(400).json({
+                    message: "Actions not found"
+                })
+            } else {
+                res.status(200).json(action)
+            }
+        })
+})
+
+//CRUD - post/Create
+router.post("/", (req, res) => {
+    Projects.insert(req.body)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: "There was an error connecting to the database"
+            })
+        })
+})
+
+//CRUD - put/Update
+router.put("/:id", (req, res) => {
+    Projects.update(req.params.id, req.body)
+        .then((project) => {
+            res.status(200).json(project);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: "Project information could not be updated"
+            })
+        });
+});
+
+//CRUD - delete
+router.delete("/:id", (req, res) => {
+    Projects.remove(req.params.id)
+        .then(project => {
+            if(project > 0) {
+                res.status(200).json({
+                    message: "Project has been successfully removed"})
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: "Project could not be removed"
+            })
+        })
+})
+module.exports = router;
